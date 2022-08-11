068c38d138b61913ede06c2c59a6f4083db00f47
tomas034t <tomasalvarez034@gmail.com>
2022-08-11 10:54:50 -0300

    conexion andando

--- backend/conexion.php
+++ backend/conexion.php
@@ -1,19 +1,13 @@
 <?php
+	$conexion = mysqli_connect('localhost', 'root', '');
+	mysqli_select_db($conexion, 'terciario');
+	if (mysqli_connect_errno()) {
 
-function conectame()
-{
-    $server = "localhost";
-    $usuario = "root";
-    $password   = "";
-    $database   = "terciario";
-    $mysqli = new mysqli($server, $usuario, $password, $database);
+		echo "Error de conexion a la Base de Datos: " . mysqli_connect_error();
+		exit();
+	} else {
+		echo "ok";
+	}
 
-    if (mysqli_connect_errno()) {
-        echo "Error" . mysqli_connect_error();
-        exit();
-    }/*else{
-			echo "Todo OK";
-		}*/
 
-    return $mysqli;
-}
+	?>
