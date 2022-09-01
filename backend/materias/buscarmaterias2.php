8b294509d3d6738b8980775a1b0222bad0323575
Charly <carloscharlywilliams@gmail.com>
2022-09-01 12:41:54 -0300

    comments

--- backend/materias/buscarmaterias2.php
+++ backend/materias/buscarmaterias2.php
@@ -7,24 +7,11 @@ $resultado = mysqli_query(conectame(), $query);
 if (!$resultado) {
     die('Query Failed' . mysqli_error(conectame()));
 } else {
-    $array["data"] = [];
+    $array["data"] = []; // declaras un array
     while ($data = mysqli_fetch_assoc($resultado)) {
         $array["data"][] = $data;
     }
-    echo json_encode($array);
+    echo json_encode($array); //el json encode te devuelve el json pero en formato string asi lo puede procesar el frontend
 }
 mysqli_free_result($resultado);
 mysqli_close(conectame());
-
-// $json = array(); // declaras un array
-// while ($row = mysqli_fetch_array($resultado)) {
-//     $json[] = array(
-//         'id' => $row['id'],
-//         'nombre' => $row['nombre'],
-//         'abreviatura' => $row['abreviatura'],
-//         'estado_m' => $row['estado_m']
-
-//     );
-// }
-// $jsonstring = json_encode($json);  //el json encode te devuelve el json pero en formato string asi lo puede procesar el frontend
-// echo $jsonstring; //devolves los datos
