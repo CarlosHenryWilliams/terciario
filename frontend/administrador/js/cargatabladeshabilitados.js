87f7136b153c5270198769df01d1c1840ef880ac
Charly <carloscharlywilliams@gmail.com>
2022-09-22 08:42:16 -0300

    arreglando texto

--- frontend/administrador/js/cargatabladeshabilitados.js
+++ frontend/administrador/js/cargatabladeshabilitados.js
@@ -30,7 +30,7 @@ var listar = function () {
       },
       {
         defaultContent:
-          "<div ><div class='btn-group'><button id='botoneditardocente' class='btn btn-primary btnEditar m-1 rounded' data-toggle='modal' data-target='#modal_plan_estudio'><i class=' fa-solid fa-pen-to-square'></i></button><button class='btn btn-success btn-sm btnEstado2 m-1 rounded'>Dar de alta <i class='fa-solid fa-upload'></i></button><button type='button' class='btn btn-secondary btn-sm btnMaterias m-1 rounded' data-toggle='modal' data-target= '#modal_materias_plan_estudio'>Matarias</button></div></div>",
+          "<div ><div class='btn-group'><button id='botoneditardocente' class='btn btn-primary btnEditar m-1 rounded' data-toggle='modal' data-target='#modal_plan_estudio'><i class=' fa-solid fa-pen-to-square'></i></button><button class='btn btn-success btn-sm btnEstado2 m-1 rounded'>Dar de alta <i class='fa-solid fa-upload'></i></button><button type='button' class='btn btn-secondary btn-sm btnMaterias m-1 rounded' data-toggle='modal' data-target= '#modal_materias_plan_estudio'>Materias</button></div></div>",
       },
     ],
 
